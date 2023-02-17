fec5b46bbfe867adc423dac596e34cf597fb1322
zhugelijuan123 <nancyrain2015@163.com>
2023-02-16 21:25:13 -0800

    assignment 5  challenge part

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -9,14 +9,7 @@ function exploreComponent() {
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
     ${NavigationSidebar("explore")}
    </div>
-   <div class="col-10 col-lg-7 col-xl-6">
-    ${ExploreComponent()}
-   </div>
 
-   <div class=" d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
-   <div class="row margin_start text-white wd-backgroundcolor-lightdark"> &nbsp;&nbsp;  Who To follow </div>
-    ${WhoToFollowList()}
-   </div>
   </div>
    `);
 }
