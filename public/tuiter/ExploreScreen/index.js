b425eee4fe9727a8bdc92c79c3e85b06d35c3909
zhugelijuan123 <nancyrain2015@163.com>
2023-02-16 21:12:17 -0800

    assignment 5  challenge part

--- public/tuiter/ExploreScreen/index.js
+++ public/tuiter/ExploreScreen/index.js
@@ -4,7 +4,7 @@ import ExploreComponent from "../ExploreScreen/ExploreComponent.js";
 
 
 function exploreComponent() {
-   $('#wd-explore').append(`
+   $('#wd-explore-new').append(`
   <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
     ${NavigationSidebar("explore")}
