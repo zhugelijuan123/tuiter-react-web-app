3297d415bf97c1ac7b6dbfcc9453b4e5dacf4eb3
zhugelijuan123 <nancyrain2015@163.com>
2023-02-16 21:37:19 -0800

    assignment 5  test explore page

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -1,14 +1,17 @@
 import NavigationSidebar from "../NavigationSidebar/index.js";
 import WhoToFollowList from "../WhoToFollowList/index.js";
+import ExploreComponent from "../ExploreScreen/ExploreComponent.js";
 
 
-function exploreComponent() {
+function explore() {
    $('#wd-explore').append(`
   <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
        ${NavigationSidebar("explore")}
       </div>
-
+       <div class="col-10 col-lg-7 col-xl-6">
+        ${ExploreComponent()}
+       </div>
       <div class=" d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
       <div class="row margin_start text-white wd-backgroundcolor-lightdark"> &nbsp;&nbsp;  Who To follow
       </div>
@@ -17,5 +20,5 @@ function exploreComponent() {
   </div>
    `);
 }
-$(exploreComponent);
+$(explore);
 
