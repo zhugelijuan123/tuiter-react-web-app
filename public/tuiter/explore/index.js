75b22dbd9118f215026857c3de0f6c859f1e5ba8
zhugelijuan123 <nancyrain2015@163.com>
2023-02-17 05:19:02 -0800

    testing

--- public/tuiter/explore/index.js
+++ public/tuiter/explore/index.js
@@ -7,15 +7,15 @@ function explore() {
    $('#wd-explore').append(`
   <div class="row mt-2">
    <div class="col-2 col-md-2 col-lg-1 col-xl-2">
-        NavigationSidebar
+        ${NavigationSidebar()}
       </div>
        <div class="col-10 col-lg-7 col-xl-6">
-       ExploreComponent
+       ${ExploreComponent()}
        </div>
       <div class=" d-none d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
       <div class="row margin_start text-white wd-backgroundcolor-lightdark"> &nbsp;&nbsp;  Who To follow
       </div>
-      WhoToFollowList
+      ${WhoToFollowList()}
       </div>
   </div>
    `);
